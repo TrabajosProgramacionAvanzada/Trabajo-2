5666294ac20c002aae5ba282ed7c74090ec7abe8
IgnacioSanhuezaZ <ignacio.sanhueza@usach.cl>
2020-01-14 15:11:12 -0300

    algo es algo

--- busquedayordenamiento.c
+++ busquedayordenamiento.c
@@ -1,45 +1,93 @@
-
-
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define limite 1000
 
 
+struct NodeD{
+  double valor;
+  unsigned long long int repeticiones;
+  struct NodeD* siguiente;
+  struct NodeD* anterior;
+};
+
+struct NodeD initNodeD(struct NodeD nodo){
+  nodo.valor = 0.0;
+  nodo.repeticiones = 0;
+  nodo->siguiente = NULL;
+  nodo->anterior = NULL;
+}
+
+struct ListD{
+  struct NodeD* head;
+  struct NodeD* mid;
+  unsigned long long int cantidad;
+};
+
+struct ListD initListD(struct ListD lista){
+  lista.cantidad = 0;
+  lista->head = initNodeD(lista->head);
+  lista->mid = head;
+}
 
 
+struct NodeD* instertND(struct  NodeD* head, double n){
+  struct NodeD* new = initNodeD(new);
+  struct NodeD* aux = head;
+  if(head == NULL){
+    new.valor = n;
+    return new;
+  }
+  else{
+    if(n < head.valor){
+      new.valor = n;
+      new->siguiente = head;
+      head->anterior = new;
+    }else{
+      if(head.valor == n){
+	head.repeticiones++;
+	return head;
+      }
+      while(aux->siguiente != NULL && n > aux->siguiente.valor){
+	aux = aux->siguiente;
+      }
+      if(aux->siguiente.valor == n){
+	aux->siguiente.repeticiones++;
+      }else{
+	new->siguiente = aux->siguiente;
+	aux->siguiente->anterior = new;
+	aux->siguiente = new;
+	new->anterior = aux;
+      }
+    }
+  }
+  return head;
+}
 
+struct ListD* instertL(struct  ListD* lista, double n){
+  lista->head = initNodeD(lista->head, n);
+  lista.cantidad++;
+  if(lista.cantidad%2 == 0 && lista.cantidad >= 4){
+    lista->mid = list->mid->siguiente;
+  }
+  return lista;
+}
 
-
-
-
-int main(){
-	
-
-	double cadena[limite];
-	double *longitud;
+int main(){	
+	double longitud;
+	double aux = 0;
 	FILE* documento;
-	int i=0;
-	documento=fopen("ArchivoA.tex", "rt");
-	fscanf(documento, "%lf", longitud);
-
-	while(&longitud!= EOF && i < limite){
-		cadena[i] = &&longitud;
-		printf("%f", cadena[i]);
-		i++;
-		fscanf(documento, "%lf", longitud);
-
-
+	int i=1;
+	documento=fopen("ArchivoA.tex", "r");
+	fscanf(documento, "%le", &longitud);
+	while(longitud != aux){
+	  printf("%le - %d\n", longitud, i);
+	  i++;
+	  aux = longitud;
+	  fscanf(documento, "%le", &longitud);
 	}
 
 	fclose(documento);
-
-	for(int i=0; i<&longitud;i++)
-	{
-		printf("%f\n",cadena[i]);
-	}
 
 	return 0;
 }
