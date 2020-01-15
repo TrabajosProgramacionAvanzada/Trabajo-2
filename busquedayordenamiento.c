4ec20404e011d32a4cd7f9b6cdc4a8c2124624de
IgnacioSanhuezaZ <ignacio.sanhueza@usach.cl>
2020-01-15 04:23:59 -0300

    Agrego el Merge sort
    Aún no funciona si, pero vamos por buen camino jeje

--- busquedayordenamiento.c
+++ busquedayordenamiento.c
@@ -54,7 +54,7 @@ node* partition(node *l, node *h){
   node *i = l->anterior; //Centinela
   node *j = NULL;
   for (j = l; j != h; j = j->siguiente){ //recorremos la lista con j 
-    if (j->valor <= x){ //si el valor actual es menor al pivote
+    if (j->valor <= x){ //si el valor actual es menor o igual al pivote
       i = (i == NULL) ? l : i->siguiente;//avanza 
       swap(&(i->valor), &(j->valor)); //intercambia los valores entre el valor límite de i con el actual
     } 
@@ -94,6 +94,69 @@ node* eliminarLista(node* head){
   return head;
 }
 
+//Función que mezcla dos listas ordenadas
+node *merge(node *first, node *second) 
+{ 
+    // If first linked list is empty 
+    if (!first) 
+        return second; 
+  
+    // If second linked list is empty 
+    if (!second) 
+        return first; 
+  
+    // Pick the smaller value 
+    if (first->valor < second->valor) 
+    { 
+        first->siguiente = merge(first->siguiente,second); 
+        first->siguiente->anterior = first; 
+        first->anterior = NULL; 
+        return first; 
+    } 
+    else
+    { 
+        second->siguiente = merge(first,second->siguiente); 
+        second->siguiente->anterior = second; 
+        second->anterior = NULL; 
+        return second; 
+    } 
+}    
+
+node* Split(node* head){ 
+  node* aux = NULL;
+  node* lento = head;//Avanza de a un nodo 
+  node* rapido = head; //Avanza de a dos nodos
+  while (rapido->siguiente != NULL && rapido->siguiente->siguiente != NULL){//avanza dos para rápido y uno de lento hasta que el rápido haya llegado al final, y por tanto el lento se posicione en la mitad 
+    rapido = rapido->siguiente->siguiente; 
+    lento = lento->siguiente; 
+  } //lento ya está en la mitad a cortar
+  aux = lento->siguiente;
+  lento->siguiente = NULL;
+  return aux;//devuelve el inicio de la segunda mitad
+}
+
+
+
+//Función que ordena por mezcla una lista
+node *MergeSort(node *head){  
+  if (!head || !head->siguiente)//Caso base si no existe la lista o tiene un sólo elemento  
+    return head;  
+  node *segundo = Split(head);  
+  head = MergeSort(head);//Sigue ordenando las mitades recurrsivamente  
+  segundo = MergeSort(segundo);    
+  return merge(head, segundo);//mezcla las dos listas  
+}
+
+void display(node* head){
+  int i = 1;
+  while (head != NULL) { 
+    printf("%le - %d \n", head->valor, i);
+    head = head->siguiente;
+    i++;
+  }
+  return;
+} 
+
 int main(){	
 	double time1 = 0.0;
 	double time2 = 0.0;
@@ -103,7 +166,7 @@ int main(){
 	node* aux = NULL;
 	FILE* documento;
 	time1 = clock();
-	documento=fopen("ArchivoC.tex", "r");
+	documento=fopen("ArchivoA.tex", "r");
 	while(1 == fscanf(documento, "%le", &numero)){
 	  head = instertND(head, numero);
 	}
@@ -111,9 +174,10 @@ int main(){
 	time1 = (clock() - time1) / CLOCKS_PER_SEC;
 	printf("lectura de archivo: %.4lf \n", time1);
 	time2 = clock();
-	QuickSort(head);
+	head = MergeSort(head);
 	time2 = (clock() - time2) / CLOCKS_PER_SEC;
-	printf("quick sort: %.4lf \n", time2);
+	printf("merge sort: %.4lf \n", time2);
+	//display(head);
 	head = eliminarLista(head);
 	return 0;
 }
