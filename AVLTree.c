8791599cf5bc9b381e5f14bf9a713247ec2f5a5b
IgnacioSanhueza <ignacio.sanhueza@usach.cl>
2020-01-15 23:13:14 -0300

    Actualización BST y AVL

--- AVLTree.c
+++ AVLTree.c
@@ -1,236 +1,176 @@
-/*
-
-			AVL TREE
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+// Estructura para el nodo del AVL
+typedef struct nodeAVL {
+  double valor;
+  struct nodeAVL *izquerda;
+  struct nodeAVL *derecha;
+  int altura;
+  long long int repeticiones;
+} nodeAVL;
 
-typedef unsigned long long int ull;
-
-typedef struct Node{
-
-	ull k=0; //Clave del nodo
-	ull r=0; //Veces que se repite la clave
-	int h=0;
-	struct Node *left;
-	struct Node *right;
-
-} node;
-
-node *insertR(node **raiz, ull key);
-node *deleteR(node **raiz, ull key);
-node *search(node **raiz, ull key);
-node *sRD(node **raiz);
-node *sRI(node **raiz);
-int maxN(ull a, ull b);
-int balance(node **raiz);
-void search(node **raiz, ull key);
-
-
-int balance(node **raiz){
-
-	return *raiz==NULL ? 0 : *raiz->left->h - *raiz->right->h;
-
+// Función que retorna la altura de un árbol
+int altura(nodeAVL *N) {
+  if (N == NULL)
+    return 0;
+  return N->altura;
 }
 
-int maxN(ull a, ull b){
+// Función que retorna el mayor entre dos double
+double max(double a, double b) { return (a > b) ? a : b; }
 
-	return a<b ? b : a
-
+// Función que inicializa un nuevo nodo AVL
+nodeAVL *newnodeAVL(double valor) {
+  nodeAVL *node = (nodeAVL *)malloc(sizeof(nodeAVL));
+  node->valor = valor;
+  node->izquerda = NULL;
+  node->derecha = NULL;
+  node->altura = 1;
+  node->repeticiones = 0;
+  return (node);
 }
 
-node *sRD(node **raiz){
-
-	node *left = *raiz->left;
-	node *right = left->right;
-
-	left->right = *raiz;
-	*raiz->left = right:
-	*raiz->h = maxN(*raiz->left->h, *raiz->right->h+1);
-	*left->h = maxN(left->left->h, left->right->h+1);
-
-	return left;
+//Función que ejecuta una rotación hacia la derecha en AVL
+nodeAVL *derechaRotate(nodeAVL *y) {
+  nodeAVL *x = y->izquerda;
+  nodeAVL *T2 = x->derecha;
+  x->derecha = y;//Rotación
+  y->izquerda = T2;
+  y->altura = max(altura(y->izquerda), altura(y->derecha)) + 1;//Actualiza las alturas
+  x->altura = max(altura(x->izquerda), altura(x->derecha)) + 1;
+  return x;
 }
 
-node *sRI(node **raiz){
-	
-	node *right = *raiz->right;
-	node *left = right->left;
-
-	right->left = *raiz;
-	*raiz->right = left;
-
-	*raiz->h = maxN(*raiz->left->h, *raiz->right->h+1);
-	*right->h = maxN(right->left->h, right->right->h+1);
-
-	return right;
-
-}
-node *insertR(node **raiz, ull key){
-
-	if(*raiz==NULL){
-
-		*raiz = malloc(sizeof(node));
-		*raiz->k = key;
-		*raiz->r = 1
-		*raiz->h = 1;
-		*raiz->left = NULL;
-		*raiz->right = NULL;
-
-		return *raiz;
-
-	}else{
-		if(*raiz->k > key){
-
-			*raiz=insertR(&*raiz->left, key);
-		
-		}else if(*raiz->k < key){
-
-			*raiz=insertR(&*raiz->right, key);
-		
-		}else if(*raiz->k == key){
-
-			*raiz->r++
-
-			return *raiz;
-		
-		}
-	}
-
-	*raiz->h = 1 + maxN(*raiz->left->h, *raiz->right->h);
-	int fB = balance(&*raiz);
-
-	if(fB > 1 && *raiz->left->k > key){
-		
-		return sRD(&*raiz);
-	
-	}
-	if(fB < -1 && *raiz->right->k < key){
-		
-		return sRI(&*raiz);
-	
-	}
-	if(fB > 1 && *raiz->right->k < key){
-		
-		*raiz->left = sRI(&*raiz->left);
-		
-		return sRD(&*raiz);
-	
-	}
-	if(fB < -1 && raiz->right->k > key){
-		
-		*raiz->right = sRD(&*raiz->right);
-		
-		return sRD(&*raiz);
-	
-	}
-
-	return *raiz;
-
+//Función que ejecuta una rotación hacia la izquerda en AVL
+nodeAVL *izquerdaRotate(nodeAVL *x) {
+  nodeAVL *y = x->derecha;
+  nodeAVL *T2 = y->izquerda;
+  y->izquerda = x;//Rotación izquerda
+  x->derecha = T2;
+  x->altura = max(altura(x->izquerda), altura(x->derecha)) + 1;//Actualiza las alturas
+  y->altura = max(altura(y->izquerda), altura(y->derecha)) + 1;
+  return y;
 }
 
-void swap(node **raiz, node **aux){
-	if(*raiz-right == NULL){
-		*aux->k = *raiz->k;
-		*aux = *raiz;
-		*a = *raiz->left;
-	}else{
-		swap(&*raiz->right, *aux);
-	}
+// Función que retorna el balance de la raíz
+int getBalance(nodeAVL *N) {
+  if (N == NULL)
+    return 0;
+  return altura(N->izquerda) - altura(N->derecha);
 }
 
-node *deleteR(node **raiz, ull key){
-
-	if(*raiz==NULL){
-		
-		return NULL;
-	
-	}else{
-
-		if(*raiz->k > key){
-
-			*raiz = deleteR(&*raiz->left, key);
-
-		}else if(*raiz->k < key){
-
-			*raiz = deleteR(&*raiz->right, key);
-
-		}else{
-
-			if(*raiz->r >= 2){
-
-				*raiz->r--;
-
-			}else{
-
-				*raiz->r--;
-				node aux = *raiz;
-
-				if(*raiz->left == NULL){
-
-					*raiz = *raiz->right;
-
-				}else if(*raiz->right == NULL){
-
-					*raiz = *raiz->left;
-
-				}else{
-
-					swap(&*raiz->left, &aux);
-
-				}
-
-				free(aux);
-			}
-		}
-	}
-
-	*raiz->h = 1 + maxN(*raiz->left->h, *raiz->right->h);
-	int fB = balance(&*raiz);
-
-	if(fB > 1 && *raiz->left->k > key){
-		
-		return sRD(&*raiz);
-	
-	}
-	if(fB < -1 && *raiz->right->k < key){
-		
-		return sRI(&*raiz);
-	
-	}
-	if(fB > 1 && *raiz->right->k < key){
-		
-		*raiz->left = sRI(&*raiz->left);
-		
-		return sRD(&*raiz);
-	
-	}
-	if(fB < -1 && raiz->right->k > key){
-		
-		*raiz->right = sRD(&*raiz->right);
-		
-		return sRD(&*raiz);
-	}
-	return *raiz;
-
+nodeAVL *insert(nodeAVL *nodeAVL, double valor) {
+  double balance = 0;
+  if (nodeAVL == NULL)//Si no hay nodo
+    return (newnodeAVL(valor));//Se inserta el nuevo nodo
+  if (valor < nodeAVL->valor)//Si el valor es menor que el de la raíz
+    nodeAVL->izquerda = insert(nodeAVL->izquerda, valor);//Se continúa insertando recursivamente hacia la izquerda
+  else if (valor > nodeAVL->valor)//de lo contrario, si es mayor
+    nodeAVL->derecha = insert(nodeAVL->derecha, valor);//Se actúa reciprocamente hacia la derecha
+  else{ //Si son iguales
+    nodeAVL->repeticiones++;//Se incrementa el contador de sus repeticiones (servirá al buscar el nodo más repetido en O(n) )
+    return nodeAVL;
+  }
+  nodeAVL->altura = 1 + max(altura(nodeAVL->izquerda), altura(nodeAVL->derecha));//Se actualiza la altira después de la insersión
+  balance = getBalance(nodeAVL);//Pide el balance para resolver según cuatro casos
+  if (balance > 1 && valor < nodeAVL->izquerda->valor)//Caso izquerda izquerda
+    return derechaRotate(nodeAVL);
+  if (balance < -1 && valor > nodeAVL->derecha->valor)//Caso derecha derecha
+    return izquerdaRotate(nodeAVL);
+  if (balance > 1 && valor > nodeAVL->izquerda->valor) {//Caso izquerda derecha
+    nodeAVL->izquerda = izquerdaRotate(nodeAVL->izquerda);
+    return derechaRotate(nodeAVL);
+  }
+  if (balance < -1 && valor < nodeAVL->derecha->valor) {//Caso derecha izquerda
+    nodeAVL->derecha = derechaRotate(nodeAVL->derecha);
+    return izquerdaRotate(nodeAVL);
+  }
+  return nodeAVL;
 }
 
-void search(node **raiz, ull key){
-	if(*raiz == NULL){
-		return;
-	}else{
-		if(*raiz->k > key){
-			search(&*raiz->left, key);
-		}
-		if(*raiz-> k < key){
-			search(&*raiz->right, key);
-		}
-		if(*raiz->k == key){
-			return;
-		}
-	}
+//Función que retorna el valor mínimo del árbol
+nodeAVL *minValuenodeAVL(nodeAVL *nodeAVL) {
+  nodeAVL *current = nodeAVL;
+  while (current->izquerda != NULL) //Hasta que no haya un nodo menor a la raíz
+    current = current->izquerda;//Se avanza
+  return current;
+}
+
+// Función que elimina un valor de un AVL
+nodeAVL *deletenodeAVL(nodeAVL *root, double valo) {
+  nodeAVL *temp = NULL;
+  nodeAVL *temp = NULL;
+  int balance = 0;
+  if (root == NULL)//Si la cabeza está vacía (caso báse)
+    return root;
+  if (valor < root->valor)//Si el valor a eliminar es menor al de la raíz
+    root->izquerda = deletenodeAVL(root->izquerda, valor);//Se continúa buscando por la izquerda
+  else if (valor > root->valor)//De lo contrario, si es mayor
+    root->derecha = deletenodeAVL(root->derecha, valor);//Se continúa por la derecha
+  else {//De lo contrario se encontró el nodo a eliminar
+    if ((root->izquerda == NULL) || (root->derecha == NULL)) {//Caso si sólo tiene un hijo
+      temp = root->izquerda ? root->izquerda : root->derecha;
+      if (temp == NULL) {//Caso sin hijos
+        temp = root;
+        root = NULL;
+      } else  //Caso con un hijo 
+        *root = *temp;
+      free(temp);
+    } else {//Caso con dos hijos
+      temp = minValuenodeAVL(root->derecha);//Se busca el elemento menor de la parte meyor
+      root->valor = temp->valor;//Copia ese valor en la raíz
+      root->derecha = deletenodeAVL(root->derecha, temp->valor);//Elimina el valor previamente copiado de las hojas menores de la derecha
+    }
+  }
+  if (root == NULL)//Si está vacío retorna
+    return root;
+  root->altura = 1 + max(altura(root->izquerda), altura(root->derecha));//Actualiza la altura del nodo
+  balance = getBalance(root);//Pide el balance del nodo y realiza uno de los 4 casos al igual que la incesrsión 
+  if (balance > 1 && getBalance(root->izquerda) >= 0)// izquerda izquerda
+    return derechaRotate(root);
+  if (balance > 1 && getBalance(root->izquerda) < 0) {// izquerda derecha
+    root->izquerda = izquerdaRotate(root->izquerda);
+    return derechaRotate(root);
+  }
+  if (balance < -1 && getBalance(root->derecha) <= 0)// derecha derecha
+    return izquerdaRotate(root);
+  if (balance < -1 && getBalance(root->derecha) > 0) {// derecha izquerda
+    root->derecha = derechaRotate(root->derecha);
+    return izquerdaRotate(root);
+  }
+
+  return root;
+}
+
+//Función que imprime en pantalla el árbol en inorder
+void inOrder(nodeAVL *root) {
+  if (root != NULL) {
+    inOrder(root->izquerda);
+    printf("%d ", root->valor);
+    inOrder(root->derecha); 
+  }
+}
+
+int main() {
+  nodeAVL *root = NULL;
+  int i = 0;
+  root = insert(root, 9);
+  root = insert(root, 5);
+  root = insert(root, 10);
+  root = insert(root, 0);
+  root = insert(root, 6);
+  root = insert(root, 11);
+  root = insert(root, -1);
+  root = insert(root, 1);
+  root = insert(root, 2);
+  printf("Inorder del AVL:\n");
+  inOrder(root);
+  scanf("%d", &i);
+  root = deletenodeAVL(root, i);
+  printf("\nPreorder traversal after deletion of %d \n", i);
+  inOrder(root);
+  scanf("%d", &i);
+  return 0;
 }
--- BST.c
+++ BST.c
@@ -1,99 +1,106 @@
-/*
-
-			BINARY SEARCH TREE
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-typedef unsigned long long int ull;
+typedef struct nodeBTS {
+  double valor;
+  struct nodeBTS *izquerda, *derecha;
+} nodeBTS;
 
-typedef struct Node{
-
-	ull k=0; //Clave del nodo
-	ull r=0; //Veces que se repite la clave
-	struct Node *left;
-	struct Node *right;
-
-} node;
-
-void insert(node **raiz, ull key);
-void delete(node **raiz, ull key);
-void swap(node **raiz, node **aux);
-void search(node **raiz, ull key);
-
-void insert(node **raiz, ull key){
-	if(*raiz==NULL){
-		*raiz = malloc(sizeof(node));
-		*raiz->k = key;
-		*raiz->left = NULL;
-		*raiz->right = NULL;
-	}else{
-		if(*raiz->k > key){
-			insert(&*raiz->left, key);
-		}
-		if(*raiz->k < key){
-			insert(&*raiz->right, key);
-		}
-		if(*raiz->k == key){
-			*raiz->r++;
-		}
-	}
+// Función que inicializa un nodo BST
+nodeBTS *newNodeBTS(double item) {
+  nodeBTS *temp = (nodeBTS *)malloc(sizeof(nodeBTS));
+  temp->valor = item;
+  temp->izquerda = temp->derecha = NULL;
+  return temp;
 }
 
-void delete(node **raiz, ull key){
-	if(*raiz == NULL){
-		return;
-	}else{
-		if(*raiz->k > key){
-			delete(&*raiz->left, key);
-		}
-		if(*raiz->k < key){
-			delete(&*raiz->right, key);
-		}
-		if(*raiz->k == key){
-			if(*raiz->r >= 2){
-				*raiz->r--;
-			}else{
-				*raiz->r--;
-				node aux = *raiz;
-				if(*raiz->left == NULL){
-					*raiz = *raiz->right;
-				}else if(*raiz->right == NULL){
-					*raiz = *raiz->left;
-				}else{
-					swap(&*raiz->left, &aux);
-				}
-				free(aux);
-			}
-		}
-	}
+//Función para imprimir en pantalla el árbol binario
+void inorder(nodeBTS *root) {
+  if (root != NULL) {
+    inorder(root->izquerda);
+    printf("%le ", root->valor);
+    inorder(root->derecha);
+  }
 }
 
-void swap(node **raiz, node **aux){
-	if(*raiz-right == NULL){
-		*aux->k = *raiz->k;
-		*aux = *raiz;
-		*a = *raiz->left;
-	}else{
-		swap(&*raiz->right, *aux);
-	}
+//Función que inserta nodo en el árbol binario
+nodeBTS *insert(nodeBTS *nodeBTS, double valor) {
+  if (nodeBTS == NULL)//Caso base, si no hay raíz
+    return newNodeBTS(valor);
+  if (valor < nodeBTS->valor)//De lo contrario seguir la recurrción hacia la izquerda si el valor a ingresar es menor al de la raíz
+    nodeBTS->izquerda = insert(nodeBTS->izquerda, valor);
+  else//De lo contrario se continúa hacia la derecha
+    nodeBTS->derecha = insert(nodeBTS->derecha, valor);
+  return nodeBTS;
 }
 
-void search(node **raiz, ull key){
-	if(*raiz == NULL){
-		return;
-	}else{
-		if(*raiz->k > key){
-			search(&*raiz->left, key);
-		}
-		if(*raiz-> k < key){
-			search(&*raiz->right, key);
-		}
-		if(*raiz->k == key){
-			return;
-		}
-	}
+//Función que devuelve el valor mínimo del árbol
+nodeBTS *minValueNodeBTS(nodeBTS *nodeBTS) {
+  nodeBTS *current = nodeBTS;
+  while (current && current->izquerda != NULL)//Hasta que no esté en la hoja más a la izquerda
+    current = current->izquerda;//Sigue a la izquerda
+  return current;
+}
+
+//Función que elimina un nodo del árbol
+nodeBTS *deleteNodeBTS(nodeBTS *root, double valor) {
+  nodeBTS *temp = NULL;
+  if (root == NULL)//Si no hay raíz (caso base)
+    return root;
+  if (valor < root->valor)//Si el valor a eliminar está a la izquerda
+    root->izquerda = deleteNodeBTS(root->izquerda, valor);//cotiua hacia la izquerda
+  else if (valor > root->valor)//De lo contrario, si es mayor
+    root->derecha = deleteNodeBTS(root->derecha, valor);//Sigue hacia la derecha
+  else {//De lo contrario se encontró el nodo a eliminar
+    if (root->izquerda == NULL) {//Caso sin hijos o uno de ambos
+      temp = root->derecha;//Se guarda
+      free(root);//Elimina directamente
+      return temp;//Devuelve el valor copiado
+    } else if (root->derecha == NULL) {//Caso recíproco al anterior
+      temp = root->izquerda;
+      free(root);
+      return temp;
+    }//Caso con dos hijos
+    temp = minValueNodeBTS(root->derecha);//Se busca el nodo menor de los mayores
+    root->valor = temp->valor;//Se copia el valor a la raíz
+    root->derecha = deleteNodeBTS(root->derecha, temp->valor);//Se elimina la hoja copiada
+  }
+  return root;
+}
+
+
+int main() {
+  nodeBTS *root = NULL;
+  int i = 0;
+  root = insert(root, 50);
+  root = insert(root, 30);
+  root = insert(root, 20);
+  root = insert(root, 40);
+  root = insert(root, 70);
+  root = insert(root, 60);
+  root = insert(root, 80);
+
+  printf("Inorder: \n");
+  inorder(root);
+  scanf("%d", &i);
+  printf("\nEliminar %d\n",i);
+  root = deleteNodeBTS(root, i);
+  printf("Inorder con %d eliminado \n", i);
+  inorder(root);
+
+  scanf("%d", &i);
+  printf("\nEliminar %d\n",i);
+  root = deleteNodeBTS(root, i);
+  printf("Inorder con %d eliminado \n", i);
+  inorder(root);
+
+  scanf("%d", &i);
+  printf("\nEliminar %d\n",i);
+  root = deleteNodeBTS(root, i);
+  printf("Inorder con %d eliminado \n", i);
+  inorder(root);
+
+  scanf("%d", &i);
+  return 0;
 }
