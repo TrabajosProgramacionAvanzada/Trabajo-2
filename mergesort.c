20d7ddea84c0df465e20dc7ad24dff8f5af5c5db
IgnacioSanhuezaZ <ignacio.sanhueza@usach.cl>
2020-01-19 00:15:03 -0300

    TODO ORDEN ESTÁ LISTO
    falta todo lo de estadística jeje

--- mergesort.c
+++ mergesort.c
@@ -54,15 +54,17 @@ int max_comp(double a, double b){
 }
 
 node *split(node *head){ 
-  node *rapido = head,*lento = head; //rápido avanza dos por cada uno que avanza el lento
-  node *aux = NULL;//auxiliar
-  while (rapido->siguiente && rapido->siguiente->siguiente){//Si existe el siguiente del rápido y el siguiente a éste 
-    rapido = rapido->siguiente->siguiente;//Avanza dos con el rápido 
-    lento = lento->siguiente; //Avanza uno con el lento
-  } //A este punto el lento debe ir en la mitad mientras el otro al fin de la lista
-  aux = lento->siguiente; //inicio de la segunda mitas
-  lento->siguiente = NULL; //corte desde primera
-  return aux; 
+	node *rapido = head,*lento = head; //rápido avanza dos por cada uno que avanza el lento
+	node *aux = NULL;//auxiliar
+	if(head){
+		while (rapido->siguiente && rapido->siguiente->siguiente){//Si existe el siguiente del rápido y el siguiente a éste 
+		rapido = rapido->siguiente->siguiente;//Avanza dos con el rápido 
+		lento = lento->siguiente; //Avanza uno con el lento
+	} //A este punto el lento debe ir en la mitad mientras el otro al fin de la lista
+	aux = lento->siguiente; //inicio de la segunda mitas
+	lento->siguiente = NULL; //corte desde primera
+  	}	
+  	return aux; 
 } 
 
 node* recorrerLista(node* head){
@@ -74,96 +76,100 @@ node* recorrerLista(node* head){
 }
 
 
-node* merge_sorted(node* A, node* B, node* head){
+node* merge_sorted(node* A, node* B){
 	node* auxA = A;
 	node* auxB = B;
+	node* head = NULL;
 	node* aux = NULL;
 	node* elim = NULL;
-	aux = recorrerLista(head);
 	if(A && B){//Si hay ambas listas para ordenar
-		while(auxB && auxA){
+		while(auxA && auxB){
+			//printf("\nwhile\n");
 			if(auxA->valor < auxB->valor){
-				if(aux){
+			//printf("\n<\n");
+				if(!head){
+					head = aux = auxA;
+					auxA = auxA->siguiente;
+					aux->siguiente = NULL;
+				}
+				else{
 					aux->siguiente = auxA;
 					auxA = auxA->siguiente;
 					aux = aux->siguiente;
-					aux->siguiente = NULL;
-				}else{
-					aux = head = auxA;
-					auxA = auxA->siguiente;
+				}
+			}else if(auxA->valor > auxB->valor){
+			//printf("\n>\n");
+				if(!head){
+					head = aux = auxB;
+					auxB = auxB->siguiente;
 					aux->siguiente = NULL;
 				}
-			}else if(auxA->valor < auxB->valor){
-				if(aux){
+				else{
 					aux->siguiente = auxB;
 					auxB = auxB->siguiente;
 					aux = aux->siguiente;
-					aux->siguiente = NULL;
-				}else{
-					aux = auxB;
+				}
+			}else{
+			//printf("\n=\n");
+				if(!head){
+					head = aux = auxA;
+					auxA = auxA->siguiente;
+					elim = auxB;
+					elim->siguiente = NULL;
 					auxB = auxB->siguiente;
 					aux->siguiente = NULL;
 				}
-			}else if(aux && auxA->valor == auxB->valor && auxB->valor != aux->valor){
-				aux->siguiente = auxA;
-				auxA = auxA->siguiente;
-				aux = aux->siguiente;
-				aux->siguiente = NULL;
-				aux->repeticion++;
-				elim = auxB;
-				auxB = auxB->siguiente;
+				else{
+					aux->siguiente = auxA;
+					auxA = auxA->siguiente;
+					elim = auxB;
+					auxB = auxB->siguiente;
+					aux = aux->siguiente;
+					aux->repeticion++;
+				}
 				free(elim);
-			}else if(aux){
-				aux->repeticion = aux->repeticion + 2;
-				elim = auxA;
-				auxA = auxA->siguiente;
-				free(elim);
-				elim = auxB;
-				auxB = auxB->siguiente;
-				free(elim);
-			}else{
-				aux = auxA;
-				auxA = auxA->siguiente;
-				elim = auxB;
-				auxB = auxB->siguiente;
-				free(elim);
-				aux->siguiente = NULL;
-				aux->repeticion++;
 			}
 		}
 		if(auxA){
-			aux->siguiente = auxA;
-			auxA = auxA->siguiente;
-			aux = aux->siguiente;
+			//printf("\nqueda A\n");
+			if(aux && head)
+				aux->siguiente = auxA;
+			else
+				head = auxA;
 		}else if(auxB){
-			aux->siguiente = auxB;
-			auxB = auxB->siguiente;
-			aux = aux->siguiente;
+			//printf("\nqueda B\n");
+			if(aux && head)
+				aux->siguiente = auxB;
+			else
+				head = auxB;
 		}
-	}
-	if(A){
-		if(aux)
-			aux->siguiente = auxA;
-		else
-			head = auxA;
-	}else if (B){
-		if(aux)
-			aux->siguiente = auxB;
-		else
-			head = auxB;
-	}
+	}else if (A){
+			//printf("\nB NULL\n");
+		return A;
+	}else{
+			//printf("\nA NULL\n");
+		return B;
+		}
 	return head;
 }
 
 
 
-node* mergeSort(node* head, node *sorted){
-	node* mitad = split(head);//caso base es el penúltimo
+node* mergeSort(node* head){
+	node* mitad = split(head);;//caso base es el penúltimo
 	node* aux1 = NULL;
-	if(head){
-		
-	}
-	return sorted;
+	node* aux2 = NULL;
+	node *sorted = NULL;
+	if(head && mitad){
+		aux1 = mergeSort(mitad);
+		aux2 = mergeSort(head);
+		sorted = merge_sorted(aux1, aux2);
+		return sorted;	
+	}else if(head)
+		return head;
+	else
+		return mitad;
+	return NULL;
 }
 
 
@@ -174,30 +180,22 @@ int main(){
 	char c = '\0';
 	node* head = NULL;
 	node* result = NULL;
-	//FILE* documento;
-	//time1 = clock();
-	// documento=fopen("ArchivoA.tex", "r");
-	// while(1 == fscanf(documento, "%le", &numero)){
-	//   head = instertND(head, numero);
-	// }
-	// fclose(documento);
-	//time1 = (clock() - time1) / CLOCKS_PER_SEC;
-	//printf("lectura de archivo: %.4lf \n", time1);
-	head = instertND(head, 1);
-	head = instertND(head, 10);
-	head = instertND(head, 5);
-	head = instertND(head, 8);
-	head = instertND(head, 2);
-	head = instertND(head, 4);
-	head = instertND(head, 6);
-	head = instertND(head, 3);
-	head = instertND(head, 9);
+	node* aux = NULL;
+	FILE* documento;
 	time1 = clock();
-	result = mergeSort(head, result);
+	documento=fopen("ArchivoA.tex", "r");
+	while(1 == fscanf(documento, "%le", &numero)){
+	  head = instertND(head, numero);
+	}
+	fclose(documento);
+	time1 = (clock() - time1) / CLOCKS_PER_SEC;
+	printf("lectura de archivo: %.4lf \n", time1);
+	time1 = clock();
+	result = mergeSort(head);
 	time1 = (clock() - time1) / CLOCKS_PER_SEC;
 	printf("merge sort: %.4lf \n", time1);
-	getchar();
-	display(head);
+	// getchar();
+	// display(result);
 	head = eliminarLista(head);
 	return 0;
 }
